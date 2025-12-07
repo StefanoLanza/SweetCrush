4d80965e296a7a586af8118acc1fa8767bd22409
Stefano Lanza <steflanz@gmail.com>
2025-12-07 22:23:02 +0100

    libSDL3.so found

--- android/SDL2_GradleApp/app/jni/Application.mk
+++ android/SDL2_GradleApp/app/jni/Application.mk
@@ -1,1 +1,9 @@
+# Use the modern, LLVM-based C++ standard library.
+# This is required for all modern NDKs (r18+).
+APP_STL := c++_shared
+
 APP_SHORT_COMMANDS := true
+
+# Optional but good practice: Define your target platform here
+# This should match your app's minSdkVersion from build.gradle.
+APP_PLATFORM := android-29 
